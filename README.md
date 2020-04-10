9092708956bdd0b48a50d495c2f84a6116bc98b3
Victor Souto <souto.victor@gmail.com>
2020-04-10 15:37:16 -0300

    initial project with static data

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # vue-blog
 Simple blog webpage with Vue.js
+
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,96 @@
+<template>
+  <transition name="post">
+    <article v-if="allReady" class="post">
+      <header class="post__header">
+        <h2 class="post__title">{{ title }}</h2>
+
+        <h3 class="post__meta">by <router-link class="post__author"
+          :to="`/by/${kebabify(author)}`">{{ author }}</router-link>
+          <span class="post__sep"></span>
+          <time>{{ prettyDate(published) }}</time>
+        </h3>
+
+        <blockquote class="post__subtitle">{{ description }}</blockquote>
+      </header>
+
+      <section class="post__body rte" v-html="content"></section>
+
+      <footer class="post__footer">
+        <vue-disqus v-if="commentsReady" shortname="vue-blog-demo"
+          :key="post" :identifier="post" :url="`https://vue-blog-demo.netlify.com/read/${post}`"/>
+      </footer>
+    </article>
+  </transition>
+</template>
+
+<script>
+import VueDisqus from 'vue-disqus/VueDisqus'
+import { kebabify, prettyDate } from '../helpers'
+
+export default {
+  name: 'blog-post',
+  resource: 'BlogPost',
+  components: { VueDisqus },
+  props: { post: String },
+
+  data() {
+    return {
+      title: '',
+      author: '',
+      content: '',
+      published: '',
+      description: '',
+      commentsReady: false,
+      ready: false
+    }
+  },
+
+  computed: {
+    allReady() {
+      return this.ready && this.post;
+    }
+  },
+
+  watch: {
+    post(to, from) {
+      if (to === from || !this.post) return;
+
+      this.commentsReady = false
+      this.$getResource('post', to)
+        .then(this.showComments)
+        .then(() => {
+          this.ready = true;
+        });
+    }
+  },
+
+  methods: {
+    kebabify,
+    prettyDate,
+    showComments() {
+      // This is injected by prerender-spa-plugin on build time, we don't prerender disqus comments.
+      if (window.__PRERENDER_INJECTED &&
+          window.__PRERENDER_INJECTED.prerendered) {
+        return;
+      }
+
+      setTimeout(() => {
+        this.commentsReady = true
+      }, 1000)
+    }
+  },
+
+  mounted() {
+    if (!this.post) {
+      this.ready = true;
+      return;
+    }
+
+    this.$getResource('post', this.post)
+      .then(this.showComments)
+      .then(() => {
+        this.ready = true;
+      });
+  }
+}
+</script>
